b9a0e83547f0ba48faccaa9f83bbc0faf3b3aaa0
RobertTorres17 <49485666+RobertTorres17@users.noreply.github.com>
2019-04-10 09:42:54 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,13 @@
+$("#replyYes").click(function() {
+    
+    $("#homers-reply").text("YES I WANT A DONUT");
+
+});
+  
+    //Use this click handler to add changes to the CSS
+$("#replyNo").click(function() {
+    
+    $("#homers-reply").text("NO I DON'T WANT A DONUT");
+    
+    
+});
